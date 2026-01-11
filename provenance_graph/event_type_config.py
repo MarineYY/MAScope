5cefe200ef70abde2a0af48e69da7b21f1f2709d
Lucas Xu <165266852+lucas-xu27@users.noreply.github.com>
2026-01-11 21:37:28 +0800

    Delete provenance_graph directory

--- provenance_graph/event_type_config.py
+++
@@ -1,9 +0,0 @@
-class LOG_TYPE:
-        AGENT_OP = ['agent_invoke', 'agent_respond']
-        FILE_OP = ['file_read', 'file_write', 'file_delete', 'file_modify', 'process_load']
-        PROCESS_OP = ['process_start', 'process_end']
-        NET_OP = ['network_send', 'network_receive']
-
-class EVENT_TYPE:
-        EVENT_OP = ['agent_invoke', 'agent_respond', 'file_read', 'file_write', 'file_delete', 'file_modify', 'process_load', 'process_start', 'process_end', 'network_send', 'network_receive']
-        
